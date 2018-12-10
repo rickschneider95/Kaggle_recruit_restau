aca1799f0a6ced3e235b68ec80b73eccd2137fc6
mdeborne <mathieu.deborne@mail.mcgill.ca>
2018-12-10 14:34:39 +0100

    shiny intro TAB

--- shiny_app/template_app1.R
+++ shiny_app/template_app1.R
@@ -6,15 +6,19 @@ library(leaflet)
 library(tidyverse)
 library(RColorBrewer)
 
-df <- read.csv("data/air_data.csv",encoding="UTF-8") 
-df <- df %>% 
-  mutate(visit_month= months(as.Date(visit_date))) %>% 
-  mutate(visit_month=as.factor(visit_month))
 
-df <- df %>% 
-  mutate_if(is.na(visit_time),visit_month= as.factor(months(as.Date(visit_date)))) 
+prediction <-read.csv("data/sub.csv",encoding="UTF-8") 
+prediction <- prediction %>%
+    separate(id, into = c('id','bin', 'visit_date'), sep = c(20,21)) %>% 
+    select(-bin) %>% 
+    mutate(visit_date=as.Date(visit_date))
 
-## if visit_time = NA pour ne pas avoir de doublon
+
+df <- read.csv("data/air_data.csv",encoding="UTF-8")
+df <- df %>% 
+  mutate( visit_month =  case_when(is.na(visit_time) ~ visit_date)) %>% 
+  mutate(visit_month= as.factor(months(as.Date(visit_month))))
+
 
 ###################################
 
@@ -46,7 +50,53 @@ ui <- fluidPage(
       
       tabsetPanel(type='tabs',
       
-      tabPanel("Intro",mainPanel()
+      tabPanel("Intro",mainPanel( width = 12,
+        h3("Welcome to our shiny app", align = "center"),
+        h4("we have 5 tabs on top  of the intro. They are"),
+        h4(" 1/ Time overview "),
+        h4(" 2/per restaurant"),
+        h4(" 3/ map "),
+        h4(" 4/by region/genre"),
+        h4(" 5/ prediction"),
+        h4(""),
+        h4("In this app, we give the user an in-depth analysis of the frequentation of restaurants 
+           in Japan. Our app can give more or less specific information, depending on the preferences of the user."),
+        h4("First, the time overview tab:"),
+        h4("Here, we don't differentiate between different restaurants. The user can enter different dates, be it months,
+           days of week, or any partition of the year. The user can choose what time period to choose, and we display 
+           the frequentation numbers for all restaurants in japan. Here of course the y-axis is very large numbers,
+           as it is the number of visitors in all restaurants for a given 'time property'."),
+        h4(""),
+        h4("Second, the per restaurant tab:"),
+        h4("Here, the user can specify the restaurant. This is more useful for the individual of course, although less so 
+            for those doing large-scale studies.So the user can specify a specific restaurant by id (more on how to get
+           that in the next tab) and then run the exact same functions as in tab 1, just now only for the restaurant
+           corresponding to that id."),
+        h4(""),
+        h4("Third, the map tab:"),
+        h4("Here we give an interactive map of japan. The user can zoom in and find the location of all the restaurants.
+           For each restaurant, we can find its id and its genre (type of restaurant) as well as its location of course.
+           This can be used in addition to the first two tabs to get a better idea of what the ids mean."),
+        h4(""),
+        h4("Fourth, the by region/genre:"),
+        h4("Here, we can get visitors info but instead of by restaurant, we get them by the region and/or genre of the
+           restaurant. What this means is we can specify the genre or region, and get visitation info based on that.
+           Ie the user can for example want to know about cocktail bars in tokyo, perhaps for a market study or personal
+           interest, and isolate the data relevant only to that specific subset.Of course, the x-axis here remains the
+           same as it has always been, namely the subset of dates fitting the required constraints set by the user,
+           but the numbers change to reflect the change of the dataset we are analysing."),
+        h4(""),
+        h4("Fifth, but not least, the prediction:"),
+        h4("The dataset we were given contained only data up to april 22nd inclusive. However, using machine learning 
+            prediction algorithms, we were able to predict the visitor data up to may 31st. This tab is that predicted data.
+            Although it cannot be said to be truthful in the barest sense of the word, it is a good prediction of what can
+            be expected. The data in this tab is forecasting future visits.It is presented in a similar way as the first 
+            tab, but with reduced capabilities due to the small timeframe of the data in question. Namely:The user can pick
+            an id and a date, and we output the predicted number of visitors for that restaurant on that day.")
+        
+        
+        
+      )
         
                ),
       
@@ -136,7 +186,7 @@ ui <- fluidPage(
                  mainPanel(
                  plotOutput("plot_time2",width = "180%",height = "600px"),
                  textOutput("number_visitors_period2")
-                 # DT::DTOutput("Table")
+                 
                )
               ),
       tabPanel("Map",
@@ -176,7 +226,15 @@ ui <- fluidPage(
                         )
         
               ),
-      tabPanel("Prediction",mainPanel()
+      tabPanel("Prediction",
+               sidebarPanel("Title",
+                            selectizeInput("id5",label = "Choose your favorite restaurant ",choices = unique(prediction$id), options = list(maxItems = 10)),
+                            dateInput("date5","Choose you date:",value = as.Date('2017-04-23'),min=as.Date('2017-04-23'),max=as.Date('2017-05-31')),
+                            actionButton("refresh5",label = "Refresh")
+                            ),
+               mainPanel(
+                          DT::DTOutput("Table5")
+                        )
                
               )
       
@@ -235,8 +293,8 @@ server <- function(input, output) {
     if(time=='visit_date'){
       df$visit_date =  as.Date(df$visit_date, format = "%Y-%m-%d")
       df <- df %>% 
-        filter(!!quo_time >= daterange[1] & !!quo_time <= daterange[2]) %>%  
-        mutate(visit_date=as.factor(visit_date))
+        filter(!!quo_time >= daterange[1] & !!quo_time <= daterange[2])  
+        #mutate(visit_date=as.factor(visit_date))
     }
     if(time=='visit_time'){
         df <- df %>% 
@@ -298,12 +356,19 @@ server <- function(input, output) {
       count(df,time,daterange)
   }
   
+  predictive_table <- function(df,ids,date){
+    df %>%
+      filter(id %in% ids) %>% 
+      filter(visit_date %in% date)
+  }
+  
    pp1 <- eventReactive(input$refresh,{general_overview(df,input$time,input$daterange)})
    pp2 <- eventReactive(input$refresh,{count(df,input$time,input$daterange)})
    pp12 <- eventReactive(input$refresh2,{personal_overview(df,input$time2,input$daterange2)})
    pp22 <- eventReactive(input$refresh2,{personal_count(df,input$time2,input$daterange2)})
    pp14 <- eventReactive(input$refresh4,{area_genre_overview(df,input$time4,input$daterange4)})
    pp24 <- eventReactive(input$refresh4,{area_genre_count(df,input$time4,input$daterange4)})
+   pp15 <- eventReactive(input$refresh5,{predictive_table(prediction,input$id5,input$date5)})
    
    output$plot_time <- renderPlot({
      pp1()
@@ -336,6 +401,8 @@ server <- function(input, output) {
    
    output$number_visitors_period4 <- renderText({ 
      glue::glue("They are {pp24()} visitors represented overall on this graph ")
+   })
+   output$Table5 <- DT::renderDT({pp15()
    })
 }
 
